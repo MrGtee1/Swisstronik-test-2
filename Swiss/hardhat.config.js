1b9dd9db5b90b6ded77dd49e77e4e3f106277dc9
MrGtee1 <141200696+MrGtee1@users.noreply.github.com>
2024-07-24 22:59:13 +0100

    Update hardhat.config.js

--- Swiss/hardhat.config.js
+++ Swiss/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox"); module.exports = {
   solidity: "0.8.19", networks: {
     swisstronik: { url: 
       "https://json-rpc.testnet.swisstronik.com/", accounts: 
-      ["0x204be920837e29185c837462f0340fb151965964a5441b2e858516fd1259e536"], 
+      ["0×"], 
       //Your private key starting with "0x"
     },
   },
